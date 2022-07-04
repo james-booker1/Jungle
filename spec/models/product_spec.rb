0eee0d55b078f8eaca6f42af900a6dba9fe3c0cc
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-07-04 14:43:09 -0700

    refactored test page for products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,36 +3,55 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     category = Category.create(name:"Extra Shrubs")
-    subject {
-    described_class.new(
-      name: "Smart",
-      price: 34.99,
-      quantity: 2,
-      category_id: category.id )
-  }
 
     it "checks product is valid" do 
-      expect(subject).to be_valid
+      product = Product.new(
+          name: "Smart",
+          price_cents: 34.99,
+          quantity: 2,
+          category_id: category.id )
+      
+      expect(product).to be_valid
     end
 
     it "checks the name" do
-      # product = Product.new(name: nil)
-      expect(subject).to be_valid
+      product = Product.new(
+          name: nil,
+          price_cents: 34.99,
+          quantity: 2,
+          category_id: category.id )
+      
+      expect(product).not_to be_valid
     end
 
-    it "checks the price" do
-      # product = Product.new(price: nil)
-      expect(subject).to be_valid
+    it "checks the price_cents" do
+      product = Product.new(
+          name: "Smart",
+          price_cents: nil,
+          quantity: 2,
+          category_id: category.id )
+      
+      expect(product).not_to be_valid
     end
 
     it "checks the quantity" do
-      # product = Product.new(quantity: nil)
-      expect(subject).to be_valid
+      product = Product.new(
+          name: "Smart",
+          price_cents: 34.99,
+          quantity: nil,
+          category_id: category.id )
+      
+      expect(product).not_to be_valid
     end
     
     it "checks the category" do
-      # product = Product.new(category: nil)
-      expect(subject).to be_valid
+      product = Product.new(
+          name: "Smart",
+          price_cents: 34.99,
+          quantity: 2,
+          category_id: nil )
+      
+      expect(product).not_to be_valid
     end
 
   end
