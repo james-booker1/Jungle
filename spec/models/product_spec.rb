04386e428f77c61891d86be2bd10e95e609ec425
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-07-04 14:00:25 -0700

    adds product tests in

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    category = Category.create(name:"Extra Shrubs")
+    subject {
+    described_class.new(
+      name: "Smart",
+      price: 34.99,
+      quantity: 2,
+      category_id: category.id )
+  }
+
+    it "checks product is valid" do 
+      expect(subject).to be_valid
+    end
+
+    it "checks the name" do
+      # product = Product.new(name: nil)
+      expect(subject).to be_valid
+    end
+
+    it "checks the price" do
+      # product = Product.new(price: nil)
+      expect(subject).to be_valid
+    end
+
+    it "checks the quantity" do
+      # product = Product.new(quantity: nil)
+      expect(subject).to be_valid
+    end
+    
+    it "checks the category" do
+      # product = Product.new(category: nil)
+      expect(subject).to be_valid
+    end
 
   end
 end
